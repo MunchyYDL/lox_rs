757b028e86ec363774d4d407c17b591ebc109f37
Marcus Christensen <marcus.christensen@polestar.com>
2023-11-19 19:45:13 +0100

    4.1 - run-file() & run-prompt()
    I also added a small test file in test/first.lox, to be able to test
    passing a file to the program, to verify that the run_file function
    worked as expected.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,49 @@
+use std::{
+    env,
+    io::{self, BufRead, Write},
+    process,
+};
+
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
+    let args: Vec<String> = env::args().collect();
 
     match args.len() {
         2 => run_file(args[1].clone()),
         1 => run_prompt(),
         _ => {
             println!("Usage: lox [script]");
-            std::process::exit(64);
+            process::exit(64);
         }
     }
 }
 
 fn run_file(path: String) {
-    println!("run_file: {path}");
+    let file_content = std::fs::read_to_string(path).unwrap(); // No error handling needed... 😛
+    run(file_content);
 }
 
 fn run_prompt() {
-    println!("run_prompt");
+    println!("Welcome to the Lox REPL");
+    println!("-----------------------");
+    println!("Ctrl-D to exit\n");
+
+    let stdin = io::stdin();
+    let mut stdout = io::stdout();
+
+    loop {
+        print!("> ");
+        stdout.flush().unwrap();
+
+        let line = stdin.lock().lines().next();
+        if line.is_none() {
+            println!("\n\nBye!");
+            break;
+        }
+
+        run(line.unwrap().unwrap());
+    }
+}
+
+fn run(content: String) {
+    println!("run:\n{content}\n");
 }
